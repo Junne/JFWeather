6e1d6ee3919084014f8efcb839c0899aee343b82
Junne <junne.bai@gmail.com>
2016-11-23 11:24:38 +0800

    add Alamofire Method

--- JFWeather/ViewController.swift
+++ JFWeather/ViewController.swift
@@ -7,36 +7,67 @@
 //
 
 import UIKit
+import SwiftyJSON
 import Alamofire
 
 class ViewController: UIViewController {
 
     let openWeatherAPIKey = "83d12c4e4664fbff167c8f114c0679a4"
-//    http://api.openweathermap.org/data/2.5/weather?q=Beijing&appid=83d12c4e4664fbff167c8f114c0679a4
+    let urlString = "http://api.openweathermap.org/data/2.5/weather?q=Beijing&appid=83d12c4e4664fbff167c8f114c0679a4"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let urlString = "http://api.openweathermap.org/data/2.5/weather?q=Beijing&appid=\(openWeatherAPIKey)"
-        let parameters : Parameters = ["weather" : "Beijing", "appid" : openWeatherAPIKey];
-//        let url = NSURL(fileURLWithPath: urlString)
         
-        Alamofire.request("http://api.openweathermap.org/data/2.5/weather", method: .get, parameters: parameters, encoding: JSONEncoding.default)
-            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
-                print("Progress: \(progress.fractionCompleted)")
+        Alamofire.request(urlString, method: .get, parameters: nil, encoding: JSONEncoding.default).validate().responseJSON { (response) -> Void in
+            print(response)
+            guard response.result.isSuccess else {
+                print("Error while fetching remote data: \(response.result.error)")
+                return
             }
-            .validate { request, response, data in
-                // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
-                return .success
-            }
-            .responseJSON { response in
-                debugPrint(response)
-                print(response)
+            
+            
+            
+            print(response.result.value ?? "Hello Value")
         }
         
         
         
-//        Alamofire.request("http://api.openweathermap.org/data/2.5/weather", method: .get , parameters: parameters, encoding: JSONEncoding.default, headers: HTTPHeaders?).validate().responseJSON {
+        
+//        let weatherURL = NSURL(string: urlString)!
+//        guard let weatherData = NSData(contentsOf: weatherURL as URL) else {
+//            return
+//        }
+//        
+//        let jsonData = JSON(data: weatherData as Data)
+//        print(jsonData)
+//        
+        
+        
+        
+//        let urlString = "http://api.openweathermap.org/data/2.5/weather?q=Beijing&appid=\(openWeatherAPIKey)"
+//        let weatherParameters = ["q": "Beijing", "appid": "83d12c4e4664fbff167c8f114c0679a4"]
+//        let weatherParameters:[String: String] = ["q": "Beijing", "appid": "83d12c4e4664fbff167c8f114c0679a4"]
+//        let url = NSURL(fileURLWithPath: urlString)
+        
+//        Alamofire.request(urlString, method: .get, parameters: nil, encoding: JSONEncoding.default)
+//            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+//                print("Progress: \(progress.fractionCompleted)")
+//            }
+//            .validate { request, response, data in
+//                // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
+//                return .success
+//            }
+//            .responseJSON { response in
+//                debugPrint(response)
+//                print(response)
+//        }
+        
+
+        
+//        Alamofire.request("http://api.openweathermap.org/data/2.5/weather", method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: HTTPHeaders?).validate().responseJSON {
+//            
 //            
 //        }
         
