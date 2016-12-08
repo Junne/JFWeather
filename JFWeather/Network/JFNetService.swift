e8f352cd9e268fa4f2405981af920b377904497a
Junne <junne.bai@gmail.com>
2016-12-08 17:33:42 +0800

    add some net code

--- JFWeather/Network/JFNetService.swift
+++ JFWeather/Network/JFNetService.swift
@@ -12,13 +12,37 @@ import Alamofire
 class JFNetService: NSObject {
     
     let openWeatherAPIKey = "83d12c4e4664fbff167c8f114c0679a4"
-    let openWeatherUrlString = "http://api.openweathermap.org/data/2.5/weather"
+    let openWeatherBaseUrlString = "http://api.openweathermap.org/data/2.5/weather"
+    let headers = ["Accept": "application/json"]
+    
+    var alamofireManager: SessionManager?
+    
+    public static var shareInstance: JFNetService {
+        struct Static {
+            static let instance: JFNetService = JFNetService()
+        }
+        return Static.instance
+    }
+    
+    override init() {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 15
+        let manager = SessionManager(configuration: config)
+        self.alamofireManager = manager
+    }
+    
+    
+
+
+}
+
+extension JFNetService {
     
     func getWeatherInfo() {
         
         let weatherParameters = ["q": "Beijing", "appid": openWeatherAPIKey]
         
-        Alamofire.request(openWeatherUrlString, method: .get, parameters: weatherParameters, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) -> Void in
+        Alamofire.request(openWeatherBaseUrlString, method: .get, parameters: weatherParameters, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
             
             guard response.result.isSuccess else {
                 print("Error while fetching remote data:\(response)")
@@ -27,5 +51,4 @@ class JFNetService: NSObject {
             print(response.result.value ?? "No Value")
         }
     }
-
 }
